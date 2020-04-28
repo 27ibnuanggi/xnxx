3004e0efe10dcecd39d6d6ac544304b48207ed39
27ibnuanggi <64445249+27ibnuanggi@users.noreply.github.com>
2020-04-28 07:15:56 +0700

    Update gas1.php

--- gas1.php
+++ gas1.php
@@ -1,106 +1,28 @@
 <?php
 
-class tri{
-  
-  function curls($host,$header,$body,$method)
-  	{
-  		$ch = curl_init();
-  		curl_setopt($ch, CURLOPT_URL, $host);
-  		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);	
-  		curl_setopt($ch, CURLOPT_HEADER, true);
-  		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-  		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  		curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
-  		curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-  		curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-  		curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-  		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-  		$req = curl_exec($ch);
-  		$req = explode("\r\n\r\n", $req);
-  		return $req;
-  	}
 
+include 'tri.php';
 
-
-  function request_otp($msisdn,$imei)
-  {
-    $body = array("msisdn"=>$msisdn);
-    $body = json_encode($body);
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/request-otp',$header,$body,'POST');
-    return $response;
-  }
-  
-  function login($msisdn,$otp)
-  {
-    $body = "grant_type".'='."password".'&'."username".'=' . $msisdn . '&'."password".'='.$otp;
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Basic Ym9uc3RyaTpib25zdHJpc2VjcmV0" ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/x-www-form-urlencoded" ,
- "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/validate-otp',$header,$body,'POST');
-    return $response[1];
-  }
-  
-  function trans($bearer)
-  {
-    $body = '{}';
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:2" ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Bearer " . $bearer,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-   $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/voucher-history",$header,$body,"POST");
-   return $response[1];
-   
-  }
-  
-  function claim($bearer,$id,$id1)
-  {
-    $body = array("rewardId"=>$id1,"rewardTransactionId"=>$id);
-    $body = json_encode($body);
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Bearer " . $bearer ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-     $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/get-voucher-code",$header,$body,"POST");
-     return $response[1];
-  }
-  
-  
-  
+$tri = new tri();
+$imei = "868880043302499";
+echo "Masukkan No Telepon : ";
+$msisdn = trim(fgets(STDIN));
+$otp = $tri->request_otp($msisdn,$imei);
+echo $otp[1] . "\r\n";
+echo "Masukkan OTP : ";
+$otp = trim(fgets(STDIN));
+$login = $tri->login($msisdn,$otp);
+$login = json_decode($login,true);
+$bearer = $login['access_token'];
+$id = $tri->trans($bearer);
+$id = json_decode($id,true);
+$id = $id['data'][0]['rewardTransactionId'];
+for($id1 = 1500; $id1 < 1600;$id1++)
+{
+  $gas = $tri->claim($bearer,$id,$id1);
+  echo $gas . "\r\n";
+  sleep(2);
 }
+
+
 ?>
